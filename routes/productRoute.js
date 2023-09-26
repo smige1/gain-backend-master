af876886bcae07d386787290b9f055e30591e912
smige1 <egimsikana@gmail.com>
2023-09-26 18:20:25 +0100

    backend master

---
+++ routes/productRoute.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+  addToWishlist,
+  rating,
+  uploadImages,
+} = require("../controller/productCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const { uploadPhoto, productImgResize } = require("../middlewares/uploadImage");
+
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createProduct);
+
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 10),
+  productImgResize,
+  uploadImages
+);
+
+// Get single product api
+router.get("/:id", getaProduct);
+
+router.put("/wishlist", authMiddleware, addToWishlist);
+
+router.put("/rating", authMiddleware, rating);
+
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+
+router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
+
+router.get("/", getAllProduct);
+
+module.exports = router;
